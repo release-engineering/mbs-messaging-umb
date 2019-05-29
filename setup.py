a3a9bc38611b50707592f62fd6f2ab56d50c12bf
Mike Bonnet <mikeb@redhat.com>
2019-05-29 07:26:37 -0700

    0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='mbs-messaging-umb',
     description='A plugin for the Module Build Service to support sending '
     'and receiving messages from the Unified Message Bus',
-    version='0.1.1',
+    version='0.1.2',
     classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Build Tools"
